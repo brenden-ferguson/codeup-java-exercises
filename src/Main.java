0f2eefc3236b5f4bd90256ec3412573f3c4bcbfa
Brenden <brenden.m.ferguson@gmail.com>
2023-06-13 19:05:42 -0500

    Completed first Java exercise

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        System.out.println("Hello world!");
+//        float myFavoriteNumber = 123;
+//        System.out.print(myFavoriteNumber);
+//        String myString = "Hello Codeup!";
+//        System.out.println(myString);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        int class;
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        int x = 4;
+//        x = x + 5;
+//        int x = 3;
+//        int y = 4;
+//        System.out.println(y*x);
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+//        System.out.println(y-(x/y));
+        System.out.print(Integer.MAX_VALUE);
+
+
+
     }
+
+
+
+
 }
