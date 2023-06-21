4f5eba11bf03aec2978e5b997781a587f1ec4ba2
Brenden <brenden.m.ferguson@gmail.com>
2023-06-21 09:40:41 -0500

    Completed OOP exercises

--- src/Shapes/Circle.java
+++ src/Shapes/Circle.java
@@ -2,5 +2,20 @@ package Shapes;
 
 public class Circle {
 
+    private Double radius;
+
+    public Circle(Double radius){
+        this.radius = radius;
+    }
+
+    public Double getArea(){
+//        area = pi x (radius ^ 2)
+        return Math.PI * (radius*radius);
+    }
+
+    public Double getCircumference(){
+//        circumference = 2 x pi x radius
+        return 2 * Math.PI * radius;
+    }
 
 }
