9b0fe2a816c9338c1f039931fa9a3ac2621c3926
Brenden <brenden.m.ferguson@gmail.com>
2023-06-26 11:45:34 -0500

    Completed the ServerNameGenerator Exercise

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,3 +1,39 @@
 public class ServerNameGenerator {
 
+    public static String randomName(){
+        String[] adjectives = {
+                "beautiful",
+                "clever",
+                "delightful",
+                "elegant",
+                "fierce",
+                "graceful",
+                "happy",
+                "incredible",
+                "joyful",
+                "kind"
+        };
+
+        String[] nouns = {
+                "cat",
+                "dog",
+                "flower",
+                "mountain",
+                "ocean",
+                "sun",
+                "tree",
+                "river",
+                "book",
+                "song"
+        };
+
+        int randomNumber = (int) Math.floor(Math.random() * 10);
+        int randomNumber2 = (int) Math.floor(Math.random() * 10);
+
+        return adjectives[randomNumber] + " " + nouns[randomNumber2];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(randomName());
+    }
 }
